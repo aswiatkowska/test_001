9d117ae6e68020150e5441cb43e82dc2552ad112
Anna Swiatkowska <a.swiatkowska177@gmail.com>
2020-10-30 22:53:59 +0100

    Functions have been added to Mother class

--- src/anna/swiatkowska/example/family/Mom.java
+++ src/anna/swiatkowska/example/family/Mom.java
@@ -1,8 +1,20 @@
 package anna.swiatkowska.example.family;
 
+import java.util.List;
+
 public class Mom extends Person {
 
     public Mom(String name, String sex, int age) {
         super(name, sex, age);
+    }
+
+    private List<Child> children;
+
+    public void addChild(String name, String sex, int age) {
+        Child child = new Child(name, sex, age);
+    }
+
+    public List<Child> getChildren() {
+        return children;
     }
 }
